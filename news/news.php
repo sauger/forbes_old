92d5c0259aa9053a78eb1150c7f57838368f8ab5
justin1986 <zhanghao@xun-ao.com>
2010-03-08 14:55:17 +0800

    log

--- news/news.php
+++ news/news.php
@@ -1,18 +1,18 @@
-<?php
-	require "../frame.php";
-?>
+<?php require_once('../frame.php');?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
-	<meta http-equiv=Content-Language content=zh-CN>
-	<title>迅傲信息</title>
-	<?php	
-		css_include_tag('/html/news/news');
-		use_jquery();
-  	?>
+	<meta http-equiv=Content-Language content=zh-cn>
+	<title>福布斯首页</title>
+	<?php
+		js_include_tag('select2css');
+		css_include_tag('top','bottom','html/news/news','select2css');
+	?>
 </head>
 <body>
 	<div id=ibody>
+		<?php include "../inc/top.inc.php";?>
 		<div id=top>
 			<div id=title>福布斯新闻</div>
 			<div id=title1><a href="">福布斯中文网</a> > <a href="">创业</a> > <a href="">物流零售</a> > <a href="" style="color:#246BB0;">华为"跨界"切入电子阅读领域</a></div>
@@ -20,72 +20,79 @@
 		</div>
 		<div id=content>
 			<div id=center-left>
-				<div id=title2>
-					<div id=t1></div><div id=t2><div class=blue></div><div class=blue></div><div id=t3><a href="" class="nor">下载PDF格式</a>   <a href="" class="nor">加入收藏</a></div></div>
+				<div id=title3>
+					华为"跨界"切入电子阅读领域
+					<div id=title2>
+						<div id=t1><a href="" class=nor5>English</a></div><div id=t2><a href="" class=nor5>我要评论(0)</a></div><a href="" class="nor">下载PDF格式</a>   <a href="" class="nor">加入收藏</a>
+					</div>
 				</div>
-				<div id=title3>华为"跨界"切入电子阅读领域</div>
+				
 				<div id=text>
+					<div id=text3>
+						面对去年营收增长265%，2010年电子书阅读器销量将达百亿美元，未来电子书籍下载将达千亿美元级别的市场，中国最大的通信设备商华为也忍不住要“跨界”。
+昨日，华为首次发布了其移动阅读集成解决方案和产品。可提供包括移动出版平台、内容分发、用户营销、手机客户端软件、手持阅读器等。研究机构DisplaySearch预测，电子阅读器市场2009年营收增长265%，全球电子阅读器市场出货将在2013年达到2000万部，而中国数字出版产业将在未来几年保持年增长率50%以上。
+  新闻出版总署科技与数字出版司数字出版处处长王强表示，预计今年数字出版产业将超过750亿元。华为软件公司副总裁黄冀透露，华为在2008年便启动了对电子阅读解决方案的研究，并一直与中国移动等保持着产业链上的合作。目前，由华为与数字出版司数字出版处处长王强表示，预计今年数字出版产业将超过750亿元。华为软件公司副总裁黄冀透露，华为在2008年便启动了对电子阅读解决方案的研究，并一直与中国移动等保持着产业链上的合作。目前，由华为承建的中国移动手机阅读业务已经于2010年1月正式商用.
+					<div id=picture6>
+					</div>黄冀预计，未来五年应该有超过30%的手机用户，通过手机和电子书来进行移动阅读，到2020年整个数字出版的销售额将占到整个出版业的一半左右，到2030年，90%的图书都将出版网络版本。国际上一些最著名的运营商都在往移动阅读和出版方向努力，例如新加坡电信等这个方向转型，这是整个业界的趋势。易观国际高级分析师张亚男表示，2010年中国手机阅读市场的活跃用户数将达到2亿人，而中国移动2009年3月建立移动阅读基地，12月份向市场推出G3的电子阅读国电信、中国联通都会涌入这个市场。
+					</div>
+					<div id=text5>
+						<div id=button>
+							<input type="button" id=button4 value="">
+						</div>
+						<div id=page>
+							Page:
+						</div>
+						<div id=num>
+							<b>1</b>|<a href="" class=nor6>2</a>|<a href=""class=nor6>3</a>|<a href=""class=nor6>4</a>|<a href=""class=nor6>5</a>|
+						</div>
+						<div id=next>
+							<a href=""class=nor6>Next></a>
+						</div>
+					</div>
 					<div id=text1>
 						<div id=square></div>
 						<div id=text2>
 							<div id=text4>
-								记者:CHANGCHENG&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;来源于:福布斯中文网&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;发布与:2010-1-21 12:53<br/>
-								见<<福布斯>>中文版杂志 2010年第二期&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;杂志发表日期:2010-1-23<br/>
+								<div class=text4-2>记者:CHANGCHENG&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;来源于:福布斯中文网</div><div class=text4-1>发布于:2010-1-21 12:53</div>
+								<div class=text4-2>见<<福布斯>>中文版杂志 2010年第二期</div><div class=text4-1>杂志发表日期:2010-1-23</div>
 								英文原稿出处:<a href="" style="text-decoration:underline;">http://www.forbes.com/2010/02/03fiscal-banking</a>
 							</div>
 						</div>
 					</div>
-					<div id=text3>
-						<div id=text3-1>
-							导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".
-						</div>
-						<div class=text3-2>导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".</div>
-						<div class=text3-2>导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".</div>
-						<div class=text3-2>导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".</div>
-						<div id=picture5></div>
-						<div id=title4>华为软件公司副总裁黄冀透露</div>					
-						<div class=text3-2>导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".</div>
-						<div id=picture6></div>
-						<div class=text3-3>导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".</div>
-						<div class=text3-3>导语:面对去年营收增长265%,2010年电子书阅读器销量将达百亿美元,未来电子书籍下载将达千亿美元级别的市场,中国最大的通信设备商华为也忍不住"跨界".导语:面对去年营收增长265%</div>
-					</div>
-					<div id=button>
-					<input type="button" value="推荐给好友">
-					</div>
 				</div>
 				<div id=right-div1>
 					<div id=right-title1>
-					<div id=tar1></div>	推荐的 投资 评论文章
-					</div>
+					<div class=tar></div>推荐的 投资 评论文章
+					</div><div id=tar1></div>
 					<div class=list1>
 					<ul>
-						<li><a href="" class="nor2">中国的出口优势不再汇在率中国的出口汇率</a></li>	
-						<li><a href="" class="nor2">中国的出口优势不再汇在率中国的出口汇率</a></li>	
-						<li><a href="" class="nor2">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
 					</ul>
 					</div>
 				</div>
 				<div id=right-div2>
 					<div id=right-title2>
-					<div id=tar2></div><div class=tar></div>关键字"创富"的评论文章
-					</div>	
+					<div class=tar></div>关键字"创富"的评论文章
+					</div><div id=tar1></div>
 					<div class=list1>
 					<ul>
-						<li><a href="" class="nor2">中国的出口优势不再汇在率中国的出口汇率</a></li>	
-						<li><a href="" class="nor2">中国的出口优势不再汇在率中国的出口汇率</a></li>	
-						<li><a href="" class="nor2">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
 					</ul>
 					</div>
 				</div>
 				<div id=right-div3>
 					<div id=right-title3>
 						<div class=tar></div>该作者的其他文章
-					</div>
+					</div><div id=tar1></div>
 					<div class=list1>
 					<ul>
-						<li><a href="" class="nor3">中国的出口优势不再汇在率中国的出口汇率</a></li>	
-						<li><a href="" class="nor3">中国的出口优势不再汇在率中国的出口汇率</a></li>	
-						<li><a href="" class="nor3">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
+						<li><a href="" class="nor4">中国的出口优势不再汇在率中国的出口汇率</a></li>	
 					</ul>
 					</div>
 				</div>
@@ -172,3 +179,7 @@
 				</div>
 			</div>
 		</div>
+		<?php include "../inc/bottom.inc.php";?>
+	</div>
+</body>
+<html>
