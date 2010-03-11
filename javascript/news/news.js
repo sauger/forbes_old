49773700bbb975e0000119f51e903ba6e3717c85
justin1986 <zhanghao@xun-ao.com>
2010-03-11 17:45:37 +0800

    news

--- javascript/news/news.js
+++ javascript/news/news.js
@@ -5,4 +5,11 @@ $(function(){
 		$(".left_top").css('display','none');
 		$("#"+$(this).attr('name')).css('display','inline');
 	});
+	
+	$(".left_bottom_title").hover(function(){
+		$(".left_bottom_title").css('background','');
+		$(this).css('background','url(/images/html/news/background2.jpg)');
+		$(".left_bottom").css('display','none');
+		$("#"+$(this).attr('name')).css('display','inline');
+	});
 });
