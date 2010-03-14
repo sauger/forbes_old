f6b510128de143d9a35d481351b4e9f21e7adbd1
robbin17 <robbin_szf@hotmail.com>
2010-03-14 17:14:10 +0800

    robbin

--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -1,15 +1,15 @@
 <div id=ibottom>
 				<div class=td1><a style="font-weight:bold;" href="">[榜单]</a><br><a href="">富豪榜</a><br><a href="">城市榜</a><br><a href="">公司榜</a><br><a href="">体育榜</a><br><a href="">名人榜</a><br><a href="">教育榜</a></div>
 				<div class=b_v></div>
-				<div class=td4><a style="font-weight:bold;" href="">[富豪]</a><br><a href="">富豪榜</a><br><a href="">富豪报道</a><br><a href="">富豪对话</a><br><a href="">图片富豪榜</a></div>
+				<div class=td1><a style="font-weight:bold;" href="">[富豪]</a><br><a href="">富豪榜</a><br><a href="">富豪报道</a><br><a href="">富豪对话</a><br><a href="">图片富豪榜</a></div>
 				<div class=b_v></div>
-				<div class=td6><a style="font-weight:bold;" href="">[投资]</a><br><a href="">慈善</a><br><a href="">保险</a><br><a href="">基金</a><br><a href="">股票</a><br><a href="">收藏</a></div>
+				<div class=td1><a style="font-weight:bold;" href="">[投资]</a><br><a href="">慈善</a><br><a href="">保险</a><br><a href="">基金</a><br><a href="">股票</a><br><a href="">收藏</a></div>
 				<div class=b_v></div>
-				<div class=td4><a style="font-weight:bold;" href="">[创业]</a><br><a href="">创业故事</a><br><a href="">创业投资</a><br><a href="">创业人物</a></div>
+				<div class=td1><a style="font-weight:bold;" href="">[创业]</a><br><a href="">创业故事</a><br><a href="">创业投资</a><br><a href="">创业人物</a></div>
 				<div class=b_v></div>
-				<div class=td7><a style="font-weight:bold;" href="">[商业]</a><br><a href="">能源重工</a><br><a href="">汽车</a><br><a href="">快速消费品</a><br><a href="">健康产业</a><br><a href="">房产</a><br><a href="">下一页>></a></div>
+				<div class=td1><a style="font-weight:bold;" href="">[商业]</a><br><a href="">能源重工</a><br><a href="">汽车</a><br><a href="">快速消费品</a><br><a href="">健康产业</a><br><a href="">房产</a><br><a href="">下一页>></a></div>
 				<div class=b_v></div>
-				<div class=td4><a style="font-weight:bold;" href="">[科技]</a><br><a href="">创新</a><br><a href="">能源</a><br><a href="">生物</a><br><a href="">医药</a><br><a href="">TNT</a></div>
+				<div class=td1><a style="font-weight:bold;" href="">[科技]</a><br><a href="">创新</a><br><a href="">能源</a><br><a href="">生物</a><br><a href="">医药</a><br><a href="">TNT</a></div>
 				<div class=b_v></div>
 				<div class=td1><a style="font-weight:bold;" href="">[城市]</a><br><a href="">城市报道<br><a href="">城市资料</a><br><a href="">城市活动</a><br><a href="">城市列表</a><br><a href="">城市特辑</a><br><a href="">城市评论</a></div>
 				<div class=b_v></div>
@@ -19,7 +19,7 @@
 				<div class=b_v></div>
 				<div class=td1><a style="font-weight:bold;" href="">[专栏]</a><br><a href="">特约记者</a><br><a href="">记者专栏</a></div>
 				<div class=b_v></div>
-				<div class=td3><a style="font-weight:bold;" href="">[会员俱乐部]</a><br><a href="">会员俱乐部介绍</a><br><a href="">读者调查</a><br><a href="">申请赠阅</a><br><a href="">会员公告</a><br><a href="">读者来函</a><br><a href="">礼品兑换</a></div>
+				<div class=td2><a style="font-weight:bold;" href="">[会员俱乐部]</a><br><a href="">会员俱乐部介绍</a><br><a href="">读者调查</a><br><a href="">申请赠阅</a><br><a href="">会员公告</a><br><a href="">读者来函</a><br><a href="">礼品兑换</a></div>
 				<div id=td5><a href="">关于福布斯中文网</a> - <a href="">新闻动态</a> - <a href="">广告服务</a> - <a href="">诚聘英才</a> - <a href="">友情连接</a> - <a href="">会员活动</a> - <a href="">隐私声明</a> - <a href="">网站声明</a> - <a href="">联系我们</a> - <a href="">网站地图</a></div>
 		</div>
 		<div id=ibabout>Copyright ? 2010 Forbes.com Inc. 福布斯公司 版权所有<br><br>备案号：户CP备000000号</div>
