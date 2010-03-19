982565a7d6f0f48d5ac7f17e7177f3abe008b8d1
shelby <sunyoujie@xun-ao.com>
2010-03-19 16:42:41 +0800

    shelby1982

--- admin/news/news_list.php
+++ admin/news/news_list.php
@@ -24,7 +24,7 @@
 		array_push($c, "set_up=$up");
 	}
 	$news = new table_class($tb_news);
-	$record = $news->paginate('all',array('conditions' => implode(' and ', $c),'order' => 'priority asc,created_at desc'),20);
+	$record = $news->paginate('all',array('conditions' => implode(' and ', $c),'order' => 'priority asc,created_at desc'),30);
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
