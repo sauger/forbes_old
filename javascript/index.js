0109ea07d628c7b31a78a520e806a50f08744684
shelby <sunyoujie@xun-ao.com>
2010-03-19 13:35:41 +0800

    shelby

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,8 @@
 var head_id=0;
-$(function(){
-	$(".head_btn2").click(function()
-	{
-		var now_id=$(this).attr('id');	
+function head(now_id)
+{
 		$(".head_btn2").css('background','url(/images/index/slideshow_unactive.gif) no-repeat');
-		$(this).css('background','url(/images/index/slideshow_active.gif) no-repeat');
+		$("#"+now_id).css('background','url(/images/index/slideshow_active.gif) no-repeat');
 	
 		$(".head_pic").hide();
 		$("#head_pic_"+now_id).show();
@@ -19,20 +17,17 @@ $(function(){
 		$("#head_related_"+now_id).show();		
 		
 		head_id=now_id;
+}
 
-	})
-	
-	$(".head_btn1").click(function()
-	{
-		var now_id=$(this).attr('id');	
-		if(now_id=="l"){now_id=parseInt(head_id)-1;}
-		else{now_id=parseInt(head_id)+1;}
+function head2()
+{
+		var now_id=head_id;	
+		now_id=parseInt(head_id)+1;
     if(now_id>3){now_id=0;}
-    if(now_id<0){now_id=3;}
 
 		$(".head_btn2").css('background','url(/images/index/slideshow_unactive.gif) no-repeat');
 		$("#"+now_id).css('background','url(/images/index/slideshow_active.gif) no-repeat');
-		
+	
 		$(".head_pic").hide();
 		$("#head_pic_"+now_id).show();
 
@@ -43,20 +38,35 @@ $(function(){
 		$("#head_content_"+now_id).show();		
 
 		$(".head_related").hide();
-		$("#head_related_"+now_id).show();	
+		$("#head_related_"+now_id).show();		
 		
 		head_id=now_id;
+		setTimeout("head2()",5000);
+}		
 
+
+
+$(function(){
+	$(".head_btn2").click(function()
+	{
+		var now_id=$(this).attr('id');	
+		head(now_id);
+	})
+	
+	$(".head_btn1").click(function()
+	{
+		var now_id=$(this).attr('id');	
+		if(now_id=="l"){now_id=parseInt(head_id)-1;}
+		else{now_id=parseInt(head_id)+1;}
+    if(now_id>3){now_id=0;}
+    if(now_id<0){now_id=3;}
+
+		head(now_id);
 	})	
 
-	
-	
-	
-	
-	
-	
+
+
 });
 
 
-
-
+setTimeout("head2()",5000);
