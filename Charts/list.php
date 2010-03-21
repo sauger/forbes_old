199db9e6a69589d552661bf6c7306d238cb6c174
robbin17 <robbin_szf@hotmail.com>
2010-03-21 17:41:24 +0800

    robbin

--- Charts/list.php
+++ Charts/list.php
@@ -1,6 +1,18 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+﻿<?php 
+	session_start();
+	require_once('../frame.php');
+	$db = get_db();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <head>
-    <link href="/css/html/sort/sort.css" rel="stylesheet" type="text/css">
+  <meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-cn>
+	<title>福布斯-榜单列表</title>
+	<?php
+		use_jquery();
+		js_include_tag('select2css');
+		css_include_tag('sort','top','bottom');
+	?>
 </head>
 <body>
 	<div id=ibody>
@@ -78,21 +90,57 @@
 			</div>
 		</div>
 		<div id=r>
+			<?php 
+				$bdid=$_REQUEST['id'];
+				switch($bdid){
+					case "1":
+						$bdname="富豪榜";
+						break;
+					case "2":
+						$bdname="投资榜";
+						break;
+					case "3":
+						$bdname="公司榜";
+						break;
+					case "4":
+						$bdname="城市榜";
+						break;
+					case "5":
+						$bdname="名人榜";
+						break;
+					case "6":
+						$bdname="体育榜";
+						break;
+					case "7":
+						$bdname="科技榜";
+						break;
+					case "8":
+						$bdname="教育榜";
+						break;
+					default: 
+			    	$bdname="常规榜"; 
+			    	break; 
+				}
+				if($bdid=="")
+				{
+					$bd=$db->paginate('select * from fb_custom_list_type order by priority asc,created_at desc',8);
+				}
+				else
+				{
+					$bd=$db->paginate('select * from fb_custom_list_type where position="'.$bdid.'" order by priority asc,created_at desc',8);
+				}
+			?>
 			<div id=r_t>
-				<div style="width:300px;float:left;display:inline;">您选择的富豪榜共有250条记录，分别如下：</div><select id=select1></select>
+				<div style="width:300px;float:left;display:inline;">您选择的<?php echo $bdname; ?>共有<?php echo count($bd); ?>条记录，分别如下：</div><select id=select1></select>
 			</div>
 			<div id=r_m>
 			</div>
 			<div id=r_b>
 				<div id=r_b_l>
-					<div id=r_b_l_t><a href="">《2010福布斯中国潜力企业榜》榜单</a></div>
-					<div id=r_b_l_t><a href="">《2010福布斯中国潜力企业榜》榜单</a></div>
-					<div id=r_b_l_t><a href="">《2010福布斯中国潜力企业榜》榜单</a></div>
-					<div id=r_b_l_t><a href="">《2010福布斯中国潜力企业榜》榜单</a></div>
-					<div id=r_b_l_t><a href="">《2010福布斯中国潜力企业榜》榜单</a></div>
-					<div id=r_b_l_t><a href="">《2010福布斯中国潜力企业榜》榜单</a></div>
-					<div id=r_b_l_t><a href="">《2010福布斯中国潜力企业榜》榜单</a></div>
-					<div id=r_b_l_t><a href="">《2010福布斯中国潜力企业榜》榜单</a></div>
+					<?php for($i=0;$i<count($bd);$i++){ ?>
+						<div class=r_b_l_t><a href=""><?php echo $bd[$i]->name; ?></a></div>
+					<?php } ?>
+					<div class=r_b_l_t><?php paginate(''); ?></div>
 				</div>
 				<div id=r_b_r>
 				</div>
