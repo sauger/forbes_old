59e950f10cc61b226e4bf25b74e47620b66a6f18
justin1986 <zhanghao@xun-ao.com>
2010-03-24 13:52:29 +0800

    fix sql

--- dbscript/change_position_relation.sql
+++ dbscript/change_position_relation.sql
@@ -1,2 +1,1 @@
-﻿ALTER TABLE `forbes`.`fb_position_relation` ADD COLUMN `type` VARCHAR(45) DEFAULT 'news' AFTER `priority`
-, ROW_FORMAT = DYNAMIC;
+﻿ALTER TABLE `forbes`.`fb_position_relation` ADD COLUMN `type` VARCHAR(45) DEFAULT 'news' AFTER `priority`;
