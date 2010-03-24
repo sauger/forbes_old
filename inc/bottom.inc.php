34b69cc17f009a24c6cc8702d5148cbbd7db9102
shelby <sunyoujie@xun-ao.com>
2010-03-24 16:50:56 +0800

    shelby1982

--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -1,9 +1,3 @@
-		<?php 
-			$path = dirname(__FILE__) .'/';
-	    	include_once($path ."../frame.php");
-	    	css_include_tag('bottom');
-	    	js_include_tag('bottom');
-		?>
 		<div id=ibottom>
 			<div id=b_top >
 				<?php $bd=$db->query('select name,href,target from fb_navigation where parent_id in (select id from fb_navigation where name="榜单" and parent_id=0) and (type="both" or type="bottom") order by priority asc'); ?>
@@ -46,3 +40,4 @@
 			<div id=td5><a href="">关于福布斯中文网</a> - <a href="">新闻动态</a> - <a href="">广告服务</a> - <a href="">诚聘英才</a> - <a href="">友情连接</a> - <a href="">会员活动</a> - <a href="">隐私声明</a> - <a href="">网站声明</a> - <a href="">联系我们</a> - <a href="">网站地图</a></div>
 		</div>
 		<div class=ibabout>Copyright @ 2010 Forbes.com Inc. 福布斯公司 版权所有<br><br>备案号：沪ICP备000000号</div>
+		
--- inc/top.inc.php
+++ inc/top.inc.php
@@ -3,14 +3,14 @@
 		$db=get_db();
 ?>
 <div id=top_>
-	<div id=top_banner><a href="#"><img border=0 src="/images/index/top_banner.jpg"></a></div>
+	<div id=top_banner><a href="#"><img border=0 src="/images/other/top_banner.jpg"></a></div>
 	<div id=top_function>
 			<div class=user_btn><a href="">登陆</a>　<a href="">注册</a></div>
 			<div class=user_btn><a href="">设为首页</a>　<a href="">收藏本页</a></div>
 			<div id=magazine_title>本期杂志介绍</div>
-			<div id=magazine_pic><a href=""><img border=0 src="/images/index/six.jpg"></a></div>
+			<div id=magazine_pic><a href=""><img border=0 src="/images/other/six.jpg"></a></div>
 			<div id=magazine_description><span class=font1>福布斯中文版VOL.22</span><br>版杂志知案例荟萃丰富的福布斯中文版杂志知识及案例...</div>
-			<div id=magazine_btn><a href=""><img src="/images/top/magazine_btn.jpg" border=0></a></div>
+			<div id=magazine_btn><a href=""><img src="/images/public/magazine_btn.jpg" border=0></a></div>
 	</div>
   <div id=top_logo>
 			<div id=border></div>
@@ -32,43 +32,43 @@
 		$navigation=$db->query('select name,id from fb_navigation where id='.$nav);
 	?>
   <div id=navigation>
-			<div class="menu" <?php if($navigation[0]->name=="首页"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu" <?php if($navigation[0]->name=="首页"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[0]->href; ?>"><div class="nav" param1="<?php echo $countnav[0]->id; ?>" id=picindex></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu"  <?php if($navigation[0]->name=="榜单"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu"  <?php if($navigation[0]->name=="榜单"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[1]->href; ?>"><div class="nav" param1="<?php echo $countnav[1]->id; ?>" id=picbangdan></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu"  <?php if($navigation[0]->name=="富豪"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu"  <?php if($navigation[0]->name=="富豪"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[2]->href; ?>"><div class="nav" param1="<?php echo $countnav[2]->id; ?>" id=picfuhao></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu" <?php if($navigation[0]->name=="投资"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu" <?php if($navigation[0]->name=="投资"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[3]->href; ?>"><div class="nav" param1="<?php echo $countnav[3]->id; ?>" id=pictouzi></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu"  <?php if($navigation[0]->name=="创业"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu"  <?php if($navigation[0]->name=="创业"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[4]->href; ?>"><div class="nav" param1="<?php echo $countnav[4]->id; ?>" id=picchuangye></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu"  <?php if($navigation[0]->name=="商业"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu"  <?php if($navigation[0]->name=="商业"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[5]->href; ?>"><div class="nav" param1="<?php echo $countnav[5]->id; ?>" id=picshangye></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu"  <?php if($navigation[0]->name=="科技"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu"  <?php if($navigation[0]->name=="科技"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[6]->href; ?>"><div class="nav" param1="<?php echo $countnav[6]->id; ?>" id=pickeji></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu"  <?php if($navigation[0]->name=="城市"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu"  <?php if($navigation[0]->name=="城市"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[7]->href; ?>"><div class="nav" param1="<?php echo $countnav[7]->id; ?>" id=picchengshi></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu"  <?php if($navigation[0]->name=="奢华"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu"  <?php if($navigation[0]->name=="奢华"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[8]->href; ?>"><div class="nav" param1="<?php echo $countnav[8]->id; ?>" id=picshehua></div></a>
 			</div>
 			<div class=vertical></div>
-			<div class="menu"  <?php if($navigation[0]->name=="专栏"){?>style="background:url('/images/top/bg_menu.jpg') repeat-x;"<?php } ?>>
+			<div class="menu"  <?php if($navigation[0]->name=="专栏"){?>style="background:url('/images/public/bg_menu.jpg') repeat-x;"<?php } ?>>
 				<a href="<?php echo $countnav[9]->href; ?>"><div class="nav" param1="<?php echo $countnav[9]->id; ?>" id=piczhuanlan></div></a>
 			</div>
 			<div id=top_function2>
