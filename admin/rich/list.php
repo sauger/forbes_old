a4cfd876b4499d8fbadfaee28222cf1b1dc29b83
sauger <sauger@163.com>
2010-03-25 11:37:59 +0800

    fix bugs

--- admin/rich/list.php
+++ admin/rich/list.php
@@ -68,7 +68,7 @@
 						<?php echo strip_tags($record[$i]->current_fortune_order);?>
 					</td>
 					<td>
-						<a href="add_to_list.php?rich_id=<?php echo $record[$i]->id;?>" class="edit" name="<?php echo $record[$i]->id;?>" title="加入榜单"><img src="/images/btn_add.png" border="0"></a>
+						<a style="display:none;" href="add_to_list.php?rich_id=<?php echo $record[$i]->id;?>" class="edit" name="<?php echo $record[$i]->id;?>" title="加入榜单"><img src="/images/btn_add.png" border="0"></a>
 						<a href="edit.php?id=<?php echo $record[$i]->id;?>" class="edit" name="<?php echo $record[$i]->id;?>" title="编辑"><img src="/images/btn_edit.png" border="0"></a>
 						<span style="cursor:pointer" class="del" name="<?php echo $record[$i]->id;?>"  title="删除"><img src="/images/btn_delete.png" border="0"></span>
 
