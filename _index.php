2e64505126baa2b32079548ae5cc3289c1767c31
shelby <sunyoujie@xun-ao.com>
2010-03-27 18:20:40 +0800

    shelby1982

--- _index.php
+++ _index.php
@@ -20,39 +20,33 @@
 			</div>
 			
 			
-			<?php 
-				$news = get_news_by_pos($catename[0]->name.'首页'.$catename[0]->name.'文章');
-			?>
-			
 			<div class=common_box>
+				<?php $news = get_news_by_pos($catename[0]->name.'首页'.$catename[0]->name.'文章');	?>
 				<div class=caption>
 					<div class=captions><?php echo $catename[0]->name; ?>文章</div>
 					<div class=line>|</div>
 					<a href="" class=more></a>
 				</div>
-					
-				<?php 
-				for($i=0;$i<count($news);$i++){ ?>
-					<div class=tz_l_b_l_t_title><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><?php echo $news[$i]->title; ?></a></div>
-					<div class=tz_l_b_l_t_content><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><?php echo strip_tags($news[$i]->description); ?></a></div>
-				<?php }
-				$news = get_news_by_pos($catename[0]->name.'首页文章');
-				?>
-
+				<?php for($i=0;$i<count($news);$i++){ ?>
+					<div class=common_article_lis1><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><?php echo $news[$i]->title; ?></a></div>
+					<div class=common_article_description1><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><?php echo strip_tags($news[$i]->description); ?></a></div>
+				<? }?>
+				
+	
+				<?php	$news = get_news_by_pos($catename[0]->name.'首页文章');	?>
 				<div class=caption>
 					<div class=captions style="width:30px;">文章</div>
 					<div class=line>|</div>
 					<a href="" class=more></a>
 				</div>
-
-
-				<?php 
-		 		 for($i=0;$i<count($news);$i++){ ?>
-					<div class=tz_l_b_l_b_content><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><?php echo $news[$i]->short_title; ?></a></div>
+				<?php  for($i=0;$i<count($news);$i++){ ?>
+					<div class=common_article_lis2><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><?php echo $news[$i]->short_title; ?></a></div>
 				<?php } ?>
 			</div>
-			<div id=tz_l_dash></div>
-			<div id=tz_l_b_r>
+			
+			<div id=common_dash></div>
+			
+			<div class=common_box2 >
 				<?php 
 					$news = get_news_by_pos($catename[0]->name.'首页'.$catename[0]->name.'专题');
 				?>
@@ -61,31 +55,32 @@
 					<div class=line>|</div>
 					<a href="" class=more></a>
 				</div>
-				<div class="tz_l_b_r_content">
-					<div class=tz_l_b_r_pic><a href="/news/news.php?id=<?php echo $news[0]->news_id; ?>"><img border=0 src="<?php echo $news[0]->photo_src; ?>"></a></div>
-					<div class=tz_l_b_r_pictitle><a href="/news/news.php?id=<?php echo $news[0]->news_id; ?>"><?php echo $news[0]->title; ?></a></div>
-					<div class=tz_l_b_r_piccontent><a href="/news/news.php?id=<?php echo $news[0]->news_id; ?>"><?php echo strip_tags($news[0]->description); ?></a></div>
+				
+				<div class=common_subject>
+					<div class=common_subject_pic><a href="/news/news.php?id=<?php echo $news[0]->news_id; ?>"><img border=0 src="<?php echo $news[0]->photo_src; ?>"></a></div>
+					<div class=common_subject_list><a href="/news/news.php?id=<?php echo $news[0]->news_id; ?>"><?php echo $news[0]->title; ?></a></div>
+					<div class=common_subject_description><a href="/news/news.php?id=<?php echo $news[0]->news_id; ?>"><?php echo strip_tags($news[0]->description); ?></a></div>
 				</div>
-				<div class="tz_l_b_r_content">
-					<div class=tz_l_b_r_pic><a href="/news/news.php?id=<?php echo $news[1]->news_id; ?>"><img border=0 src="<?php echo $news[1]->photo_src; ?>"></a></div>
-					<div class=tz_l_b_r_pictitle><a href="/news/news.php?id=<?php echo $news[1]->news_id; ?>"><?php echo $news[1]->title; ?></a></div>
-					<div class=tz_l_b_r_piccontent><a href="/news/news.php?id=<?php echo $news[1]->news_id; ?>"><?php echo strip_tags($news[1]->description); ?></a></div>
+				<div class=common_subject>
+					<div class=common_subject_pic><a href="/news/news.php?id=<?php echo $news[1]->news_id; ?>"><img border=0 src="<?php echo $news[1]->photo_src; ?>"></a></div>
+					<div class=common_subject_list><a href="/news/news.php?id=<?php echo $news[1]->news_id; ?>"><?php echo $news[1]->title; ?></a></div>
+					<div class=common_subject_description><a href="/news/news.php?id=<?php echo $news[1]->news_id; ?>"><?php echo strip_tags($news[1]->description); ?></a></div>
 				</div>
+				
 				<?php for($i=2;$i<count($news);$i++){ ?>
-					<div class=tz_l_b_l_b_content><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>">·<?php echo $news[$i]->short_title; ?></a></div>
-				<?php }
-				$news = get_news_by_pos($catename[0]->name.'首页'.$catename[0]->name.'专栏');
-				?>
-				<div class=caption>
+					<div class=common_article_lis2><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><?php echo $news[$i]->short_title; ?></a></div>
+				<?php }?>
+				
+				<? $news = get_news_by_pos($catename[0]->name.'首页'.$catename[0]->name.'专栏');	?>
+				<div class=caption style="margin-top:20px;">
 					<div class=captions><?php echo $catename[0]->name; ?>专栏</div>
 					<div class=line>|</div>
 					<a href="" class=more></a>
 				</div>
 				<?php for($i=0;$i<count($news);$i++){ ?>
-				<div class=tz_l_b_r_b>
-					<a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><span style="color:#003899; font-size:14px; font-weight:bold;"><?php echo $news[$i]->title; ?></span><br><?php strip_tags($news[$i]->description); ?></a>	
-				</div>
-				<div class=tz_l_b_r_b_zl>——<?php echo $news[$i]->author; ?>专栏</div>
+				<div class=common_list3><a href="/news/news.php?id=<?php echo $news[$i]->news_id; ?>"><?php echo $news[$i]->short_title; ?></a></div>
+				<div class=common_description3><?php echo strip_tags($news[$i]->description); ?></div>
+				<div class=common_writer>——<?php echo $news[$i]->author; ?>专栏</div>
 				<?php } ?>
 			</div>
 		</div>
