9d981d9a9a21a1e0d42999b5eee18a0117668d05
sauger <sauger@163.com>
2010-03-31 16:04:39 +0800

    fix bugs

--- inc/project_pubfun.php
+++ inc/project_pubfun.php
@@ -19,11 +19,11 @@ function get_news_by_pos($pos,$page='') {
 		$category_id=$record[0]->category_id;
 		$all_category_ids = $category->children_map($category_id);
 		$all_category_ids = implode(',',$all_category_ids);
-		$sql = 'select n.created_at,n.id as id,n.id as news_id,n.title,n.short_title,n.video_photo_src,n.description,n.sub_headline,n.category_id from fb_news n left join fb_category c on n.category_id=c.id where c.id in('.$all_category_ids.') and c.category_type="news" order by n.created_at desc limit '.$record[0]->position_limit;
+		$sql = 'select n.created_at,n.id as id,n.id as news_id,n.title,n.short_title,n.video_photo_src,n.description,n.sub_headline,n.category_id from fb_news n left join fb_category c on n.category_id=c.id where n.is_adopt=1 and c.id in('.$all_category_ids.') and c.category_type="news" order by n.created_at desc limit '.$record[0]->position_limit;
 	}
 	elseif($record[0]->type=="news")
 	 {
-	 	$sql='select n.created_at,n.id as id,n.id as news_id,n.title,n.short_title,n.video_photo_src,n.description,n.sub_headline,n.author_id from fb_position_relation f left join fb_news n on f.news_id=n.id where  f.type="news" and f.position_id='.$record[0]->id.' order by f.priority limit '.$record[0]->position_limit;
+	 	$sql='select n.created_at,n.id as id,n.id as news_id,n.title,n.short_title,n.video_photo_src,n.description,n.sub_headline,n.author_id from fb_position_relation f left join fb_news n on f.news_id=n.id where n.is_adopt=1 and f.type="news" and f.position_id='.$record[0]->id.' order by f.priority limit '.$record[0]->position_limit;
 	 }
 	 elseif($record[0]->type=="list"){
 	 	$sql = "select n.id,n.name,n.image_src,n.comment from fb_position_relation f join fb_custom_list_type n on f.news_id=n.id where f.position_id={$record[0]->id} and f.type='list' order by f.priority limit {$record[0]->position_limit}";
