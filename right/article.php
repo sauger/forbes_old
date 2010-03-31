3628c31099e851f9b0808f139c16d76cc2d48787
justin1986 <zhanghao@xun-ao.com>
2010-03-31 14:10:38 +0800

    news

---
+++ right/article.php
@@ -0,0 +1,27 @@
+<div id=hy_r_b_title>
+	<div id=wz>福布斯精华文章定制</div>	
+</div>
+<div id=hy_r_b_left></div>
+<div id=hy_r_b_center>
+	<div id=content>
+		<div id=content_t>
+			<div id=content_t_l>
+				订阅福布斯快闻　<input type="radio"><span style="font-size:12px; font-weight:normal; color:#666666;">我要定制</span>	
+			</div>
+			<div id=content_t_r>
+				<button>定制</button>	
+			</div>	
+		</div>
+		<div id=content_b>
+			<div id=content_b_l>
+				<span style="font-size:14px; color:#11579e; font-weight:bold;">订阅分类文章</span>
+				<div class=cl><input type="radio">富豪　<input type="radio">创业　<input type="radio">商业</div>
+				<div class=cl><input type="radio">投资　<input type="radio">生活</div>
+			</div>
+			<div id=content_b_r>
+				<button>定制</button>	
+			</div>	
+		</div>	
+	</div>	
+</div>
+<div id=hy_r_b_right></div>
