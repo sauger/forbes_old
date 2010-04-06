596fd84bc89df721ba71fd20f3197ab9c620b9b5
justin1986 <zhanghao@xun-ao.com>
2010-04-06 18:14:23 +0800

    position

--- admin/position/update_pos.php
+++ admin/position/update_pos.php
@@ -1,4 +1,63 @@
 <?php
 include_once "../../frame.php";
-$db = get_db();
+
+
+
+
+function update_pos($category_name,$limit,$position_name,$is_parent=false){
+	$db = get_db();
+	$category = new category_class();
+	if($is_parent){
+		$category_id = $category->find_by_name($category_name)->id;
+		$ids = $category->children_map($category_id);
+		$ids = implode(",",$ids);
+		$sql = "select id,title,short_title,created_at,description,video_photo_src from fb_news where 1=1 and is_adopt=1 and category_id in ($ids) order by created_at desc";
+	}else{
+		$category_id = $category->find_by_name($category_name)->id;
+		$sql = "select id,title,short_title,created_at,description,video_photo_src from fb_news where 1=1 and is_adopt=1 and category_id={$category_id} order by created_at desc";
+	}
+	//echo $sql;
+	$news = $db->query($sql);
+	$count = $db->record_count;
+	for($i=0;$i<$count;$i++){
+		for($j=0;$j<$limit;$j++){
+			$pos_name = $position_name.$j;
+			$record = $db->query("select id,end_time from fb_page_pos where name='{$pos_name}'");
+			echo "select id,end_time from fb_page_pos where name='{$pos_name}'";
+			if($db->record_count==1){
+				if($record[0]->end_time>now()){
+				}else{
+					$pos = new table_class('fb_page_pos');
+					$pos->find($record[0]->id);
+					$end_time = date('Y-m-d H:00:00',strtotime("+1hours", time()));
+					$pos->end_time = $end_time;
+					$pos->display = $news[$i]->short_title;
+					$pos->title = $news[$i]->titile;
+					$pos->image1 = $news[$i]->video_photo_src;
+					$pos->description = $news[$i]->description;
+					$pos->href = dynamic_news_url($news[$i]);
+					$pos->static_href = static_news_url($news[$i]);
+					$pos->save();
+					break;
+				}
+			}else{
+				$pos = new table_class('fb_page_pos');
+				$pos->name = $pos_name;
+				$end_time = date('Y-m-d H:00:00',strtotime("+1hours", time()));
+				$pos->end_time = $end_time;
+				$pos->display = $news[$i]->short_titile;
+				$pos->title = $news[$i]->titile;
+				$pos->image1 = $news[$i]->video_photo_src;
+				$pos->description = $news[$i]->description;
+				$pos->href = dynamic_news_url($news[$i]);
+				$pos->static_href = static_news_url($news[$i]);
+				$pos->comment = $category_name.$i;
+				$pos->save();
+				break;
+			}
+		}
+	}
+}
+
+
 include "./_index.php";
