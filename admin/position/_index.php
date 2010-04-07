b747608e41703bc61a85fc88fef23ee0c6235173
justin1986 <zhanghao@xun-ao.com>
2010-04-07 15:18:35 +0800

    index

--- admin/position/_index.php
+++ admin/position/_index.php
@@ -1,6 +1,6 @@
 <?php
 
-update_pos("陆家嘴早餐",3,"index_bf");
+#update_pos("陆家嘴早餐",3,"index_bf");
 update_pos("创业",4,"index_bus",true);
 update_pos("商业",5,"index_business",true);
 update_pos("科技",5,"index_tech",true);
