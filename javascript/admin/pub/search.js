70fa99f006ea91c7d0c5dfa47d5a440fcfb12d3c
sauger <sauger@163.com>
2010-04-09 11:03:20 +0800

    fix bugs

--- javascript/admin/pub/search.js
+++ javascript/admin/pub/search.js
@@ -11,9 +11,19 @@ function search_news(){
 	$('.'+class_name).each(function(){
 		url.push($(this).attr('name') + '=' + encodeURI($(this).val()));
 	});
-	
+	var filter_category = $('.category_select:last').val();
+	var category_count = $('.category_select').length;
+	if(filter_category == -1){
+		if(category_count < 2){
+			
+		}else{
+			filter_category = $('.category_select:eq('+ (category_count - 2) + ')').val();
+		}
+	}
+	url.push('category='+filter_category);
 	url = "?" + url.join('&');
 	//var url = "?title="+encodeURI($("#title").attr('value'))+"&category="+$("#category").attr('value')+"&adopt="+$("#adopt").attr('value');
 	//url += "&language_tag=" + $('#language_tag').val();
 	window.location.href=url;	
+	
 }
