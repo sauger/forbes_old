1c9c420393dcac8a059f4b11fdeeb8cdfbd6e211
shelby <sunyoujie@xun-ao.com>
2010-04-12 16:39:43 +0800

    shelby1982

--- index.php
+++ index.php
@@ -57,12 +57,12 @@
 	
 					<div id=more></div>
 					<div id=btn>
-						<div class=headline_btn1 id=l style="background:url(images/index/slideshow_back.gif) no-repeat;"></div>
-						<div class=headline_btn2 id=0 style="background:url(images/index/slideshow_active.gif) no-repeat"></div>
-						<div class=headline_btn2 id=1 style="background:url(images/index/slideshow_unactive.gif) no-repeat"></div>
-						<div class=headline_btn2 id=2 style="background:url(images/index/slideshow_unactive.gif) no-repeat"></div>
-						<div class=headline_btn2 id=3 style="background:url(images/index/slideshow_unactive.gif) no-repeat"></div>
-						<div class=headline_btn1 id=r style="background:url(images/index/slideshow_next.gif) no-repeat"></div>
+						<div class=headline_btn1 id=l style="background:url(/images/index/slideshow_back.gif) no-repeat;"></div>
+						<div class=headline_btn2 id=0 style="background:url(/images/index/slideshow_active.gif) no-repeat"></div>
+						<div class=headline_btn2 id=1 style="background:url(/images/index/slideshow_unactive.gif) no-repeat"></div>
+						<div class=headline_btn2 id=2 style="background:url(/images/index/slideshow_unactive.gif) no-repeat"></div>
+						<div class=headline_btn2 id=3 style="background:url(/images/index/slideshow_unactive.gif) no-repeat"></div>
+						<div class=headline_btn1 id=r style="background:url(/images/index/slideshow_next.gif) no-repeat"></div>
 					</div>
 				</div>
 			</div>
