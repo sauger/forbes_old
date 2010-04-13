87496ba48c85e1199ab4733a88a0128ffa604c23
justin1986 <zhanghao@xun-ao.com>
2010-04-13 18:20:50 +0800

    fix list

--- admin/position/_listindex.php
+++ admin/position/_listindex.php
@@ -41,14 +41,14 @@ function update_list_pos($pos){
 	global $db;
 	$table = new table_class('fb_page_pos');
 	$type = pos_type($pos);
-	$items = $db->query("select name,comment,image_src from fb_custom_list_type where position = {$pos} order by created_at desc limit 4");
+	$items = $db->query("select name,comment,image_src,id from fb_custom_list_type where position = {$pos} order by created_at desc limit 4");
 	$exist_items = $table->find('all',array('conditions' => "name like 'listindex_{$type}%' and (end_time <= now() or end_time is null)",'order' =>"name"));
 	
 	$len = empty($exist_items)? 0 : count($exist_items);
 	for($i=0;$i<$len; $i++){
 		$exist_items[$i]->display = $items[$i]->name;
 		$exist_items[$i]->description = $items[$i]->comment;
-		$exist_items[$i]->href= '/list/show_list.php?id' .$items[$i]->id;
+		$exist_items[$i]->href= '/list/show_list.php?id='.$items[$i]->id;
 		$exist_items[$i]->image1 = $items[$i]->image_src;
 		$exist_items[$i]->title = $items[$i]->name;
 		$exist_items[$i]->end_time = dt_increase(1,'h',$exist_itmes->end_time);
