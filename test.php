5387f72497187bc156b724311362a8fcfafa01e6
sauger <sauger@163.com>
2010-04-15 13:41:37 +0800

    fix bugs

--- test.php
+++ test.php
@@ -10,28 +10,19 @@
 		?>
 	</head>
 	<body>
-		<div style="float:left">aba</div>
-		<div style="border:1px solid;float:left;width:500px;">
-			<div style="width:50px;height:50px;background-color:green;margin-left:10px;margin-right:10px;">
-				<div style="width:30px;height:30px;background-color:gray;margin-left:5px;"></div>
-			</div>
-		</div>
+		<div id="debug"></div>
+		<div id="hover" style="width:100px; height:100px;background-color:red;"></div>
 	</body>
 </html>
 
 <script>
-	//$('#editor').val('ok');	
+	//$('#editor').val('ok');
+	var i =0;	
 	$(function(){
-		var tags = ['php','asp','jsp'];
-		$('#auto').autocomplete({source:'sauger.php'});
-		function show(){
-			$window = $(window);
-			var str = new Array();
-			 str.push('windowheith=' + $window.height());
-			str.push('scrolltop=' + $window.scrollTop());
-			alert(str); 
-			
-		}
+		$('#hover').hover(function(){
+			i = i + 1;
+			$('#debug').html(i);
+		});
 	});
 </script>
 
